0484f600cae819378c396fa122e81ff1fe4c4ad7
Krunal Rathod <rathod.krunal.rk@gmail.com>
2026-08-03 23:40:14 +0530

    save

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,32 @@
+
+import Image from 'next/image'
+import React from 'react'
+import { assets } from '../../assets/assets'
+const Navbar = () => {
+  return (
+    <>
+    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+      <Image src={assets.header_bg_color} alt='' className='w-full'/>
+    </div>
+
+    <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
+      <a href="">
+        <Image src={assets.logo} alt='' className='w-28 cursor-pointer mr-14'/>
+      </a>
+      <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white/50 shadow-sm '>
+        <li><a className='font-ovo' href="#top">Home</a></li>
+        <li><a className='font-ovo' href="#about">About me</a></li>
+        <li><a className='font-ovo' href="#services">Services</a></li>
+        <li><a className='font-ovo' href="#work">My Work</a></li>
+        <li><a className='font-ovo' href="#contact">Contact me</a></li>
+      </ul>
+      <div>
+        <a href="#contact" className='hidden lg:flex gap-3 items-center px-10 py-2.5 border border-gray-500 rounded-full ml-4'>Contact <Image src={assets.arrow_icon} alt='' className='w-3'/></a>
+      </div>
+    </nav>
+      
+    </>
+  )
+}
+
+export default Navbar
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,14 @@ import { Outfit, Ovo } from "next/font/google";
 import "./globals.css";
 
 const outfit = Outfit({
-  subsets: ["latin"], weight:["400", "500", "600" , "700"]
+  subsets: ["latin"], weight:["400", "500", "600" , "700"],
+  variable: "--font-outfit",
+   
 });
 
 const ovo = Ovo({
-  subsets: ["latin"], weight:["400"]
+  subsets: ["latin"], weight:["400"],
+   variable: "--font-ovo",
 });
 
 export const metadata: Metadata = {
@@ -23,7 +26,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${outfit.className} ${ovo.className} h-full antialiased`}
+      className={`${outfit.variable} ${ovo.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
